49aa63b909a43c8727264be0df96796383c11378
멍멍민 <pr620718@cu.ac.kr>
2025-07-11 11:05:59 +0900

    🚀 feat: DummyClient

---
+++ Server/DummyClient/Packet/PacketHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        //if (chatPacket.PlayerId == 1)
+            //Console.WriteLine(chatPacket.Chat);       
+    }
+}
