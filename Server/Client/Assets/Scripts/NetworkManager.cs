f3c0b2e3c81fe29c808a97a7895c04030c84456b
멍멍민 <pr620718@cu.ac.kr>
2025-07-11 13:15:54 +0900

    🚀 feat: Client

---
+++ Server/Client/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NetworkManager : MonoBehaviour
+{
+    ServerSession _session = new ServerSession();
+
+    void Start()
+    {
+        // DNS (Domain Name System)
+        string host = Dns.GetHostName();
+        IPHostEntry ipHost = Dns.GetHostEntry(host);
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+        Connector connector = new Connector();
+
+        connector.Connect(endPoint, () => { return _session; }, 1);
+
+        StartCoroutine(CoSendPacket());
+    }
+
+    void Update()
+    {
+        IPacket packet = PacketQueue.Instance.Pop();
+        if (packet != null)
+        {
+            PacketManager.Instance.HandlePacket(_session, packet);
+        }
+    }
+
+    IEnumerator CoSendPacket()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3.0f);
+
+            C_Chat chatPacket = new C_Chat();
+            chatPacket.Chat = "Hello Unity !";
+            ArraySegment<byte> segment = chatPacket.Write();
+
+            _session.Send(segment);
+        }
+    }
+}
---
+++ Server/Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        //if (chatPacket.PlayerId == 1)
+        {
+            Debug.Log(chatPacket.Chat);
+
+            GameObject go = GameObject.Find("Player");
+            if (go == null)
+                Debug.Log("Player not found");
+            else
+                Debug.Log("Player found");
+        }
+        //if (chatPacket.PlayerId == 1)
+        //Console.WriteLine(chatPacket.Chat);       
+    }
+}
